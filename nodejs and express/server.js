0d67ec54b8cfac3e20ee0317ea478b1d42559528
unknown <YY413@ccs.local>
2025-03-14 19:03:03 +0800

    adding nodejs and express

--- nodejs and express/server.js
+++ nodejs and express/server.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const app = express();
 
-// Define the /test route
 app.get('/test', (req, res) => {
-  res.json({ message: 'Express is working! Write your full name' });
+  res.json({ message: 'Express is working! John Jecu N.Cutanda!!' });
 });
 
-// Start the server on port 3000
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
 });
